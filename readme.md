3b33a34c7d407302c0202a676328ba9c7dccccc5
Kiwy <kiwycodealchemist@mailfence.com>
2023-07-14 15:35:08 +0200

    Premier commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Minuteur
+
+Ce projet existe car il me fallait un minuteur et que cela me faisait un bon entrainement pour ma formation actuelle en code.
+
+## Licence 
+CC BY-NC-SA
---
+++ script.js
@@ -0,0 +1,33 @@
+//On récupère le bouton
+const button = document.getElementById('button');
+//On récupère l'audio
+const audio = document.getElementById('audio');
+
+//On crée la fonction de timer.
+const timer = () => {
+    //On récupère la valeur de l'input et on transforme en nombre
+    let time = Number(document.getElementById('minutes').value);
+
+    //on vérifie bien que le paramètre récupéré est un nombre
+    if (typeof time === 'number') {
+        //Converstion des minutes en milisecondes
+        time *= 60000;
+
+        //On démare le chargement de la musique
+        audio.load();
+
+        //On démare le timer
+        setTimeout(() => {
+            audio.volume = 0.75;
+            audio.play();
+        }, time);
+
+    } else {
+        //Si la valeur n'est pas un nombre on change le texte du label afin de demander un nombre.
+        document.getElementById('label').innerHTML = 'Veillez choisir un nombre pour que le minuteur fonctionne !'
+    }
+}
+
+
+//Création de l'évènement
+button.addEventListener('click', timer);
